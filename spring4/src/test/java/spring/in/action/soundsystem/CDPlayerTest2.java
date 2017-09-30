55c07d0135118d700b5c4e34ec5841a22b4cf7d8
fiboll <fiboll@o2.pl>
2017-09-30 20:19:37 +0200

    Verifying automatic configuration

---
+++ spring4/src/test/java/spring/in/action/soundsystem/CDPlayerTest2.java
@@ -0,0 +1,37 @@
+package spring.in.action.soundsystem;
+
+import static org.junit.Assert.*;
+
+import org.junit.contrib.java.lang.system.StandardOutputStreamLog;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(classes=CDPlayerConfig.class)
+public class CDPlayerTest2 {
+    @Rule
+    public final StandardOutputStreamLog log = new StandardOutputStreamLog();
+    @Autowired
+    private MediaPlayer player;
+
+    @Autowired
+    @Qualifier("lonelyHeartsClub")
+    private CompactDisc cd;
+    @Test
+    public void cdShouldNotBeNull() {
+        assertNotNull(cd);
+    }
+    @Test
+    public void play() {
+        player.play();
+            assertEquals(
+                "Playing Sgt. Pepper's Lonely Hearts Club Band" +
+                        " by The Beatles\n",
+                log.getLog());
+    }
+}
