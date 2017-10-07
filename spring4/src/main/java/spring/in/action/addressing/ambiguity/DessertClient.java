e4257ad460a3c49c22361e59f528d1e33cfba5c4
fiboll <fiboll@o2.pl>
2017-10-07 19:40:39 +0200

    Scoping beans

--- spring4/src/main/java/spring/in/action/addressing/ambiguity/DessertClient.java
+++ spring4/src/main/java/spring/in/action/addressing/ambiguity/DessertClient.java
@@ -1,13 +1,15 @@
 package spring.in.action.addressing.ambiguity;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
-import sun.security.krb5.internal.crypto.Des;
 
 /**
  * Created by private on 07.10.17.
  */
 @Component
+@Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
 public class DessertClient {
     @Autowired
     private Dessert dessert;
