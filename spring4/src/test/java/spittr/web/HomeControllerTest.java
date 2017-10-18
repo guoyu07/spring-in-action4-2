08ff9fc5dc803c3e5db3626a29d4c72f101d4d94
dawid strembicki <fiboll@o2.pl>
2017-10-18 17:01:23 +0200

    add spittles view

--- spring4/src/test/java/spittr/web/HomeControllerTest.java
+++ spring4/src/test/java/spittr/web/HomeControllerTest.java
@@ -1,11 +1,19 @@
 package spittr.web;
 
-import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.*;
-import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
-import static org.springframework.test.web.servlet.setup.MockMvcBuilders.*;
-
+import org.hamcrest.Matchers;
 import org.junit.Test;
+import org.mockito.Mockito;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+import org.springframework.test.web.servlet.setup.MockMvcBuilders;
+import org.springframework.web.servlet.view.InternalResourceView;
+import spittr.data.Spittle;
+import spittr.data.SpittleRepository;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
 
 
 public class HomeControllerTest {
@@ -13,9 +21,36 @@ public class HomeControllerTest {
     @Test
     public void testHomePage() throws Exception {
         HomeController controller = new HomeController();
-        MockMvc mockMvc = standaloneSetup(controller).build();
-        mockMvc.perform(get("/"))
-                .andExpect(view().name("home"));
-
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller).build();
+        mockMvc.perform(MockMvcRequestBuilders.get("/"))
+                .andExpect(MockMvcResultMatchers.view().name("home"));
     }
+
+    @Test
+    public void shouldShowRecentSpittles() throws Exception {
+        List<Spittle> expectedSpittles = createSpittleList(20);
+        SpittleRepository mockRepository = Mockito.mock(SpittleRepository.class);
+        Mockito.when(mockRepository.findSpittles(Long.MAX_VALUE, 20))
+                .thenReturn(expectedSpittles);
+        SpittleController controller = new SpittleController(mockRepository);
+
+        MockMvc mockMvc = MockMvcBuilders.standaloneSetup(controller)
+                .setSingleView(
+                        new InternalResourceView("/WEB-INF/views/spittles.jsp"))
+                .build();
+        mockMvc.perform(MockMvcRequestBuilders.get("/spittles"))
+                .andExpect(MockMvcResultMatchers.view().name("spittles"))
+                .andExpect(MockMvcResultMatchers.model().attributeExists("spittleList"))
+                .andExpect(MockMvcResultMatchers.model().attribute("spittleList",
+                        Matchers.hasItems(expectedSpittles.toArray())));
+    }
+
+    private List<Spittle> createSpittleList(int count) {
+        List<Spittle> spittles = new ArrayList<>();
+        for (int i=0; i < count; i++) {
+            spittles.add(new Spittle("Spittle " + i, new Date()));
+        }
+        return spittles;
+    }
+
 }
